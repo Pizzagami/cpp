7a07546dddc7bb822dc4401ae241502de2e62281
Pizzagami <pizzagami@pizzagami.fr>
2021-02-19 10:36:27 +0100

    pull

--- cpp00/ex01/contact.class.cpp
+++ cpp00/ex01/contact.class.cpp
@@ -5,34 +5,34 @@
 void	Contact::new_contact(void)
 {
 	std::cout << "First Name : ";
-	std::getline(std::cin, this->first_name);
+	std::cin >> this->_first_name;
 	
 	std::cout << "Last Name : ";
-	std::getline(std::cin, this->last_name);
+	std::cin >> this->_last_name;
 	
 	std::cout << "Nickname : ";
-	std::getline(std::cin, this->nickname);
+	std::cin >> this->_nickname;
 	
 	std::cout << "login : ";
-	std::getline(std::cin, this->login);
+	std::getline(std::cin, this->_login);
 	
 	std::cout << "Phone Number : ";
-	std::getline(std::cin, this->phone_number);
+	std::cin, this->_phone_number;
 	
 	std::cout << "Postal Address : ";
-	std::getline(std::cin, this->postal_address);
+	std::getline(std::cin, this->_postal_address);
 	
 	std::cout << "Birthday : ";
-	std::getline(std::cin, this->bday);
+	std::getline(std::cin, this->_bday);
 	
 	std::cout << "Favorite meal : ";
-	std::getline(std::cin, this->favorite_meal);
+	std::getline(std::cin, this->_favorite_meal);
 	
 	std::cout << "Underwear color : ";
-	std::getline(std::cin, this->underwear_color);
+	std::getline(std::cin, this->_underwear_color);
 	
 	std::cout << "Darkest secret : ";
-	std::getline(std::cin, this->darkest_secret);
+	std::getline(std::cin, this->_darkest_secret);
 
 	std::cout << "Contact ajouté !" << std::endl;
 	return;
@@ -44,24 +44,24 @@ void	Contact::search_contact(int i)const{
 	int j;
 
 	std::cout<< i << "         |";
-	str = this->first_name;
-	if (this->first_name.length() > 10)
+	str = this->_first_name;
+	if (this->_first_name.length() > 10)
 		str = str.substr(0, 9) + std::string(".");
 	j = str.length();
 	while(j++ < 10)
 		str += std::string(" ");
 	str += std::string("|");
 	std::cout << str;
-	str = this->last_name;
-	if (this->last_name.length() > 10)
+	str = this->_last_name;
+	if (this->_last_name.length() > 10)
 		str = str.substr(0, 9) + std::string(".");
 	j = str.length();
 	while(j++ < 10)
 		str += std::string(" ");
 	str += std::string("|");
 	std::cout << str;
-	str = this->nickname;
-	if (this->nickname.length() > 10)
+	str = this->_nickname;
+	if (this->_nickname.length() > 10)
 		str = str.substr(0, 9) + std::string(".");
 	j = str.length();
 	std::cout << str << std::endl;
@@ -71,33 +71,33 @@ void	Contact::search_contact(int i)const{
 void	Contact::show_contact(void) const{
 
 	std::cout << "First Name : ";
-	std::cout << this->first_name;
+	std::cout << this->_first_name;
 	std::cout << std::endl;
 	std::cout << "Last Name : ";
-	std::cout << this->last_name;
+	std::cout << this->_last_name;
 	std::cout << std::endl;
 	std::cout << "Nickname : ";
-	std::cout << this->nickname;
+	std::cout << this->_nickname;
 	std::cout << std::endl;
 	std::cout << "login : ";
-	std::cout << this->login;
+	std::cout << this->_login;
 	std::cout << std::endl;
 	std::cout << "Phone Number : ";
-	std::cout << this->phone_number;
+	std::cout << this->_phone_number;
 	std::cout << std::endl;
 	std::cout << "Postal Address : ";
-	std::cout << this->postal_address;
+	std::cout << this->_postal_address;
 	std::cout << std::endl;
 	std::cout << "Birthday : ";
-	std::cout << this->bday;
+	std::cout << this->_bday;
 	std::cout << std::endl;
 	std::cout << "Favorite meal : ";
-	std::cout << this->favorite_meal;
+	std::cout << this->_favorite_meal;
 	std::cout << std::endl;
 	std::cout << "Underwear color : ";
-	std::cout << this->underwear_color;
+	std::cout << this->_underwear_color;
 	std::cout << std::endl;
 	std::cout << "Darkest secret : ";
-	std::cout << this->darkest_secret;
+	std::cout << this->_darkest_secret;
 	std::cout << std::endl;
 }
