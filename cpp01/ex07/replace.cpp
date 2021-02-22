b3ba860fb15901a9d93b0db72d029ef4b73d67b0
pizzagami <selgrabl@student.42.fr>
2021-02-22 21:02:58 +0100

    pizza

--- cpp01/ex07/replace.cpp
+++ cpp01/ex07/replace.cpp
@@ -2,12 +2,24 @@
 #include <string>
 #include <fstream>
 
-int main(int argc, char **argv)
+std::string str_replace_all(std::string line, std::string str1, std::string str2)
+{
+	size_t	pos = 0;
+
+	while((pos = line.find(str1, pos)) != std::string::npos)
+	{
+		line.replace(pos, str1.length(), str2);
+		pos += str2.length();
+	}
+	return line;
+}
+
+int			main(int argc, char **argv)
 {
 	std::string		str1 = argv[2];
 	std::string		str2 = argv[3];
 	std::ifstream	ifile(argv[1]);
-	std::string		str;
+	std::string		line;
 	if (argc != 4)
 	{
 		std::cout << " Error nombre d'arguments incorrect ! " << std::endl;
@@ -21,9 +33,8 @@ int main(int argc, char **argv)
 	else
 	{
 		std::ofstream ofile("FILENAME.replace");
-		str << ifile;
-		std::replace(str.begin(), str(end), str1, str2);
-		ofile << line;
+		while(std::getline(ifile, line))
+			ofile << str_replace_all(line, str1, str2) << "\n";
 		ifile.close();
 		ofile.close();
 	}
