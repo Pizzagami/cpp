ff2c9671aa1c24d8037a983501c7dd34802b1cea
Samuel ELGRABLY <selgrabl@e2r1p4.42.fr>
2021-03-03 14:11:41 +0100

    c1pap mac

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -2,14 +2,16 @@
 
 int     main(void)
 {
-    std::string pony_stack;
-    std::string pony_heap;
+	std::string pony_stack;
+	std::string pony_heap;
 
-    std::cout << "Entrez le noms du premier poney : ";
-    std::cin >> pony_stack;
-    std::cout << "Entrez le noms du deuxieme poney : ";
-    std::cin >> pony_heap;
-    ponyOnTheStack(pony_stack);
-    ponyOnTheHeap(pony_heap);
-    return 0;
+	std::cout << "Entrez le noms du premier poney : ";
+	while (pony_stack.length() < 1)
+		std::getline(std::cin, pony_stack);
+	std::cout << "Entrez le noms du deuxieme poney : ";
+	while (pony_heap.length() < 1)
+		std::getline(std::cin, pony_heap);
+	ponyOnTheStack(pony_stack);
+	ponyOnTheHeap(pony_heap);
+	return 0;
 }
