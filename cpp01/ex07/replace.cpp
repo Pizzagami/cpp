b809e3143ffafe3d3d72bb7ae77e7156f055b9aa
Samuel ELGRABLY <selgrabl@e2r1p4.42.fr>
2021-03-03 14:30:15 +0100

    vf

--- cpp01/ex07/replace.cpp
+++ cpp01/ex07/replace.cpp
@@ -16,16 +16,18 @@ std::string str_replace_all(std::string line, std::string str1, std::string str2
 
 int			main(int argc, char **argv)
 {
-	std::string		str  = argv[1];
-	std::string		str1 = argv[2];
-	std::string		str2 = argv[3];
-	std::ifstream	ifile(argv[1]);
-	std::string		line;
 	if (argc != 4)
 	{
 		std::cout << " Error nombre d'arguments incorrect ! " << std::endl;
 		return 0;
 	}
+
+	std::string		str  = argv[1];
+	std::string		str1 = argv[2];
+	std::string		str2 = argv[3];
+	std::ifstream	ifile(argv[1]);
+	std::string		line;
+
 	if (!(ifile.is_open()))
 	{
 		std::cout << "Probleme avec le file : " << argv[1] << std::endl;
